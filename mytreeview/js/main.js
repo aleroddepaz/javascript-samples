28c84ebd91df386da3df674c9cb820583fd37dc3
aleroddepaz <alexrdp90@gmail.com>
2017-04-13 20:19:38 +0200

    Get jquery and bootstrap from cdn

--- mytreeview/js/main.js
+++ mytreeview/js/main.js
@@ -1,8 +1,8 @@
 requirejs.config({
   baseUrl: 'js',
   paths: {
-    jquery: 'lib/jquery-2.1.4',
-    bootstrap: 'lib/bootstrap'
+    jquery: 'https://code.jquery.com/jquery-3.2.1.min',
+    bootstrap: 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min'
   },
   shim: {
     bootstrap: [ 'jquery' ]
